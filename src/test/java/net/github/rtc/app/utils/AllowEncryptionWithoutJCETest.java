c76d9756a5cd9e7581ac862205faaf778ca95fab
r0ndom <r0ndom@yahoo.com>
2015-03-12 19:12:09 +0200

    checkstyle

--- src/test/java/net/github/rtc/app/utils/AllowEncryptionWithoutJCETest.java
+++ src/test/java/net/github/rtc/app/utils/AllowEncryptionWithoutJCETest.java
@@ -1,20 +1,15 @@
 package net.github.rtc.app.utils;
 
-import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.lang.reflect.Field;
 
-import static com.sun.javaws.JnlpxArgs.verify;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
+import static junit.framework.Assert.assertEquals;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class AllowEncryptionWithoutJCETest {
