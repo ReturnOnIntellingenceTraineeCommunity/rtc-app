5082911e15303c69e817e0f80aa78d65214f3b0e
r0ndom <LeNeMu260>
2015-03-13 12:54:25 +0200

    trying to debug test

--- src/test/java/net/github/rtc/app/utils/AllowEncryptionWithoutJCETest.java
+++ src/test/java/net/github/rtc/app/utils/AllowEncryptionWithoutJCETest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 
@@ -13,6 +15,8 @@ import static junit.framework.Assert.assertEquals;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class AllowEncryptionWithoutJCETest {
+
+    private static Logger log = LoggerFactory.getLogger(AllowEncryptionWithoutJCETest.class.getName());
 
     @InjectMocks
     private AllowEncryptionWithoutJCE allowEncryptionWithoutJCE;
@@ -26,7 +30,13 @@ public class AllowEncryptionWithoutJCETest {
     public void testAfterPropertiesSet() throws Exception {
         final Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
         field.setAccessible(true);
+        log.debug("BEFORE:" + field.toString());
+        System.out.println("BEFORE:" + field.toString());
+        log.debug("BEFORE:" + field.get(null).toString());
+        System.out.println("BEFORE:" + field.get(null).toString());
         allowEncryptionWithoutJCE.afterPropertiesSet();
+        log.debug("AFTER:" + field.get(null).toString());
+        System.out.println("AFTER:" + field.get(null).toString());
         assertEquals(false, field.get(null));
     }
 }
